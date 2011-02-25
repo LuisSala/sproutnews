78e7dee4e8c9c66a5a72f638f50d0cc141d3e357
Luis Sala <lsala@primogen.com>
2011-02-25 15:40:10 -0800

    Initial sprout news work.

---
+++ apps/sprout_news/models/item.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   SproutNews.Item
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals SproutNews */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+SproutNews.Item = SC.Record.extend(
+/** @scope SproutNews.Item.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
